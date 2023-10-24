374019376d29cf293234164bbc792391bba3028e
SamsonWandah <samsonwandah@gmail.com>
2023-10-24 16:45:37 +0300

    Roles

---
+++ resources/views/components/role/permission.vue
@@ -0,0 +1,125 @@
+<template>
+    <div @click="launchForm">
+        <slot name="trigger">
+        </slot>
+    </div>
+    <teleport to="body">
+        <Transition
+            enter-from-class="opacity-0 scale-125"
+            enter-to-class="opacity-100 scale-100"
+            enter-active-class="transition duration-300"
+            leave-active-class="transition duration-200"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-125"
+        >
+            <div @click.self="show=false" v-if="show" class="inset-0 fixed bg-black bg-opacity-40 z-[100000] grid place-items-center p-5">
+                <div class="bg-white lg:w-3/5 w-full rounded-xl overflow-hidden">
+                    <header class="p-3 bg-gray-100">
+                        <div class="flex justify-between">
+                            <div class="font-medium text-sumo-300 text-lg">
+                               Edit Permissions
+                            </div>
+                            <div>
+                                <div class="text-end">
+                                    <button type="button" @click="show=false" class="rounded-full bg-sky-700 h-7 w-7 flex place-content-center">
+                                        <svg class="h-5 fill-white self-center" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M312.1 375c9.369 9.369 9.369 24.57 0 33.94s-24.57 9.369-33.94 0L160 289.9l-119 119c-9.369 9.369-24.57 9.369-33.94 0s-9.369-24.57 0-33.94L126.1 256L7.027 136.1c-9.369-9.369-9.369-24.57 0-33.94s24.57-9.369 33.94 0L160 222.1l119-119c9.369-9.369 24.57-9.369 33.94 0s9.369 24.57 0 33.94L193.9 256L312.1 375z"/></svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                    <div class="px-10 py-8 custom-scrolling overflow-y-auto max-h-[400px] m-2">
+                        <form @submit.prevent="submit">
+                            <div class="grid grid-cols-1 gap-1 my-5">
+                                <div>
+                                    <input type="checkbox" class="m-3 sumo-check" id="products" v-model="form.products">
+                                    <label class="sumo-label" for="products">Products</label>
+                                    <div v-if="form.errors.products" class="mt-3 text-red-800 text-sm">
+                                        <span class="text-xs">{{form.errors.products }}</span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="checkbox" class="m-3 sumo-check" id="machine" v-model="form.machine">
+                                    <label class="sumo-label" for="machine">Machine</label>
+                                    <div v-if="form.errors.machine" class="mt-3 text-red-800 text-sm">
+                                        <span class="text-xs">{{form.errors.machine }}</span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="checkbox" class="m-3 sumo-check" id="shifts" v-model="form.shifts">
+                                    <label class="sumo-label" for="shifts">Shifts</label>
+                                    <div v-if="form.errors.shifts" class="mt-3 text-red-800 text-sm">
+                                        <span class="text-xs">{{form.errors.shifts }}</span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <input type="checkbox" class="m-3 sumo-check" id="reading" v-model="form.reading">
+                                    <label class="sumo-label" for="reading">Reading</label>
+                                    <div v-if="form.errors.reading" class="mt-3 text-red-800 text-sm">
+                                        <span class="text-xs">{{form.errors.reading }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="my-3 flex justify-end">
+                                <button type="submit" class="btn-primary">Update <span v-show="form.processing" class="ml-2 animate-ping"><i class="fa-solid fa-ellipsis"></i></span></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Transition>
+    </teleport>
+</template>
+
+<script setup lang="ts">
+import {useForm} from "@inertiajs/vue3";
+import {ref} from "vue";
+
+let props=defineProps({
+    role:Object,
+    permissions:Object
+})
+
+let show=ref(false)
+
+let form=useForm({
+    'role':'',
+    'products': '',
+    'machine': '',
+    'shifts': '',
+    'reading': '',
+})
+const perms=()=>{
+    props.permissions.forEach((score) => {
+        if(score.name == 'products'){
+            form.products=true
+        }
+        if(score.name == 'machine'){
+            form.machine=true
+        }
+        if(score.name == 'shifts'){
+            form.shifts=true
+        }
+        if(score.name == 'reading'){
+            form.reading=true
+        }
+    });
+}
+const launchForm=()=>{
+    perms();
+    form.role=props.role?.name
+    show.value=true
+}
+const submit=()=>{
+    form.post(route('admin.roles.permission'),{
+        onSuccess:()=>{
+            show.value = false
+            form.reset()
+        }
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/layouts/admin.vue
+++ resources/views/layouts/admin.vue
@@ -166,7 +166,17 @@ const logout=()=>{
                         <span class="ml-3">Users</span>
                     </Link>
                 </li>
+                <li>
+                    <Link :href="route('admin.roles.index')" class="flex items-center p-2 text-gray-900 rounded-lg hover:text-sky-700  hover:bg-gray-100 group"
+                          :class="{ 'bg-gray-100 text-sky-700': $page.component === 'admin/roles/index' }"
+                    >
+                        <svg class="h-5 fill-gray-900 group-hover:fill-sky-700"  :class="{ 'fill-sky-700': $page.component === 'admin/roles/index' }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                          <path d="M384 192h-32V127.1c0-70.58-57.42-127.1-128-127.1s-128 57.42-128 127.1V192H64C28.65 192 0 220.7 0 256v191.1c0 35.35 28.65 64 64 64h320c35.35 0 64-28.65 64-64V256C448 220.7 419.3 192 384 192zM144 128c0-44.11 35.89-80 80-80s80 35.89 80 80v64h-160V128zM400 448c0 8.822-7.178 16-16 16H64c-8.822 0-16-7.178-16-16V256c0-8.822 7.178-16 16-16h320c8.822 0 16 7.178 16 16V448z"/>
+                        </svg>
 
+                        <span class="ml-3">Roles</span>
+                    </Link>
+                </li>
 
             </ul>
             <div class="my-5">
